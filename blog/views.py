7bc5a95ec0a28da9d31a37bd0676af895fb8c6e0
Ajay Mishra <ajumishra5@gmail.com>
2026-02-05 04:32:28 +0530

    Dark Mode implmented

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 from .models import Post
+from .forms import PostForm
+from django.shortcuts import redirect
 
 def post_list(request):
     posts = Post.objects.all().order_by('-created_at')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def create_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('post_list')
+    else:
+        form = PostForm()
+
+    return render(request, 'blog/create_post.html', {'form': form})
